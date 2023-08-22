439123353fb7d5479f4496949336f256d9932e89
Deepak <dr1872002@gamil.com>
2023-08-23 00:09:14 +0530

    changes

--- form.js
+++ form.js
@@ -1,6 +1,9 @@
+
 document.addEventListener("click", myFunction);
 function myFunction() {
     let fname = document.getElementById("name").value;
     let fage = document.getElementById("age").value;
+    let block = document.getElementById("block");
+   
     document.getElementById("block").innerHTML=("Hello "+fname+ ", You are "+fage+ " year old.")
     }
